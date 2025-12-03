3b703a764c70bc394966a87eb109b456a465cffa
cristycr <cristy.ca.rubio@gmail.com>
2025-12-03 10:20:41 +0100

    idk

--- SCRIPTS/bash_p08/stats_select.sh
+++ SCRIPTS/bash_p08/stats_select.sh
@@ -13,4 +13,58 @@
 # 2) Promedio por clase
 # 3) Fin
 
+fichero="alumnos.txt";
 
+function REGISTROS() {
+	read -p "Clase ? " clase
+
+	grep "|$clase|" "$fichero" |
+	while read linea; do
+		nom=$(printf "%s" "$linea" | cut -d"|" -f1);
+		cla=$(printf "%s" "$linea" | cut -d"|" -f2);
+		not=$(printf "%s" "$linea" | cut -d"|" -f3);
+
+		printf "%-12s %-6s %-5s\n" "$nom" "$cla" "$not";
+	done
+};
+
+function PROMEDIO() {
+	read -p "Clase ? " clase;
+
+	total=0;
+	conta=0;
+
+	while read linea; do
+		nota=$(printf "%s" "$linea" | cut -d"|" -f3);
+
+		printf "Nota : %s\n" "$nota";
+		total=$(( total + nota ));
+		conta=$(( conta + 1 ));
+	done < <(grep "|$clase|" "$fichero");
+
+	if [[ conta -gt 0 ]]; then
+		prome=$(( total / conta ));
+		echo "Promedio de la clase de $clase: $prome";
+	else
+		echo "No hay información de la clase $clase";
+	fi
+};
+
+PS3="Su opcion: ";
+
+select opt in "Extracto por clase" "Promedio por clase" "Fin"; do
+	case $REPLY in
+		1)
+			REGISTROS;
+		;;
+		2)
+			PROMEDIO;
+		;;
+		3)
+			exit 0;
+		;;
+		*)
+			echo "Opción no válida";
+		;;
+	esac
+done
---
+++ SCRIPTS/bash_p08/tabAsoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Crea un script que cuente el número de habitantes por ciudad.
+
+# 
